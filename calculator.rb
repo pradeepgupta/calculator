2a897d7daf4e38e8fe428db267b6f08ebd8a7f7a
pradeepgupta <pradeepc2c@yahoo.com>
2013-04-06 19:01:53 +0530

    Final version

--- calculator.rb
+++ calculator.rb
@@ -8,25 +8,39 @@ num2 = gets.chomp
 
 puts "Your number is #{num1} and #{num2}"
 
+
+while true
+
 puts "What you wants to do?"
 puts "Press 1 for add"
 puts "Press 2 for subtraction"
 puts "Press 3 for multiplication"
 puts "Press 4 for division"
 
+
 response = gets.chomp
 
-case response
-	when "1"
-		answer = num1.to_i + num2.to_i
-	when "2"
-		answer = num1.to_i - num2.to_i
-	when "3"
-		answer = num1.to_i * num2.to_i
-	when "4"
-		answer = num1.to_f / num2.to_f
-	else
-		puts "Wrong number pressed"
+if response == "1"
+answer = num1.to_i + num2.to_i
+break
+
+elsif response == "2"
+answer = num1.to_i - num2.to_i
+break
+
+elsif response == "3"
+answer = num1.to_i * num2.to_i
+break
+
+elsif response == "4"
+answer = num1.to_f / num2.to_f
+break
+
+else
+	puts "Please choose correct number"
+	puts
 end
+end
+
 
 puts "Answer: #{answer}"
